a58f6d83f660c3a9835d98d5f3caa2a291eab6bc
Quantao <75652473+quantaosun@users.noreply.github.com>
2021-12-18 15:01:24 +0800

    modifiy based on your resources

--- 2.2.sh
+++ 2.2.sh
@@ -1,26 +1,15 @@
 module load intel/18.0.1.163 openmpi/3.1.2-intel namd/2.13-mpi
 
-export Honey_NAMD=/apps/namd/2.13-mpi/arch/Linux-x86_64-icc   # this need to be modified
+export /apps/namd/2.13-mpi/arch/Linux-x86_64-icc:$PATH  # this need to be modified
 
 ###################################################################
-mpirun --oversubscribe -np 4 $Fuck_NAMD/namd2 +ppn 7 nvt_equil_modi.namd > nvt_equil_modi.out &&
+mpirun --oversubscribe -np 4 namd2 +ppn 7 nvt_equil_modi.namd > nvt_equil_modi.out &&
 
 #NPT###############################################################
 
-mpirun --oversubscribe -np 4 $Fuck_NAMD/namd2 +ppn 7 npt_equil_modi.namd > npt_equil_modi.out &&
+mpirun --oversubscribe -np 4 namd2 +ppn 7 npt_equil_modi.namd > npt_equil_modi.out &&
 
 # Production#######################################################
 
-mpirun --oversubscribe -np 4 $Fuck_NAMD/namd2 +ppn 7 md_forward_modi.namd > md_forward_modi.out &&
-
-cd ../../solvent/solvent-input-files &&
-
-mpirun --oversubscribe -np 4 $Fuck_NAMD/namd2 +ppn 7 nvt_equil_modi.namd > nvt_equil_modi.out &&
-
-#NPT###############################################################
-
-mpirun --oversubscribe -np 4 $Fuck_NAMD/namd2 +ppn 7 npt_equil_modi.namd > npt_equil_modi.out &&
-
-# Production#######################################################
-
-mpirun --oversubscribe -np 4 $Fuck_NAMD/namd2 +ppn 7 md_forward_modi.namd > md_forward_modi.out 
+mpirun --oversubscribe -np 4 namd2 +ppn 7 md_forward_modi.namd > md_forward_modi.out &&
+  
